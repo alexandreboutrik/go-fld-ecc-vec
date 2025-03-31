e653cf683c1f0498023d46e3911444e22b27cc3a
Boutrik <alexandreboutrik@protonmail.ch>
2025-03-31 16:04:29 -0300

    fix: make it an update for ./build/

--- update.sh
+++ update.sh
@@ -8,11 +8,27 @@ function apply_patch() {
     sed -i "s,static int ed25519_verify,extern int ed25519_verify,g" src/sign255.c
 }
 
-git clone https://github.com/armfazh/fld-ecc-vec
-cd fld-ecc-vec
-apply_patch
-mkdir build; cd build
-cmake ..
-time make all
-cd lib
-sudo cp ./* /usr/local/lib
+function compile_eddsa_avx() {
+    if [ ! -d "./fld-ecc-vec" ] ; then
+        git clone https://github.com/armfazh/fld-ecc-vec
+    fi
+
+    cd fld-ecc-vec
+    apply_patch
+    mkdir build; cd build
+    cmake .. ||
+    { echo "Failed to compile fld-ecc-vec. Cmake. Exiting" ; exit 1 ;}
+    time make all ||
+    { echo "Failed to compile fld-ecc-vec. Make. Exiting" ; exit 1 ;}
+    cd ..; cd ..
+}
+
+function copy_libs() {
+    mkdir -p ./build
+    cp -v ./fld-ecc-vec/build/lib/* ./build/
+}
+
+compile_eddsa_avx
+copy_libs
+
+rm -rf ./fld-ecc-vec
