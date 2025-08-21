4182add8ef63a2c87e0cf8c3d0c116270d7f4aa4
alexandreboutrik <alexandreboutrik@protonmail.ch>
2025-08-22 01:35:54 +0200

    fix: rename SHAKE symbols to avoid conflicts
    Renames the SHAKE128 and SHAKE256 symbols within libfaz_ecc_avx2.a. This workaround prevents symbol conflicts when linking against other libraries that also define these symbols.

--- update.sh
+++ update.sh
@@ -8,6 +8,21 @@ function apply_patch() {
     sed -i "s,static int ed25519_verify,extern int ed25519_verify,g" src/sign255.c
 }
 
+function rename_obj() {
+	mkdir tmp && cd tmp || exit 1
+
+	ar x ../lib/libfaz_ecc_avx2.a || exit 1
+	for file in *.o ; do
+		objcopy --redefine-sym SHAKE128=faz_SHAKE128 \
+				--redefine-sym SHAKE256=faz_SHAKE256 \
+				"${file}"
+	done
+
+	ar rcs ../lib/libfaz_ecc_avx2.a *.o && cd .. || exit 1
+
+	echo "Symbols renamed successfully."
+}
+
 function compile_eddsa_avx() {
     if [ ! -d "./fld-ecc-vec" ] ; then
         git clone https://github.com/armfazh/fld-ecc-vec
@@ -20,6 +35,9 @@ function compile_eddsa_avx() {
     { echo "Failed to compile fld-ecc-vec. Cmake. Exiting" ; exit 1 ;}
     time make all ||
     { echo "Failed to compile fld-ecc-vec. Make. Exiting" ; exit 1 ;}
+
+	rename_obj
+
     cd ..; cd ..
 }
 
